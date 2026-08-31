0b7af21a8272d7ba9d0387c8c098bb57cfd9b97e
Keyayco <1.19308269e+08+Keyayco@users.noreply.github.com>
2026-08-31 14:08:14 +0000

    fix: export Vercel Express handler natively

--- api/trpc/[...path].ts
+++ api/trpc/[...path].ts
@@ -1,19 +1,5 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { createApiApp } from "../../server/_core/api";
 
-let appPromise: Promise<ReturnType<typeof createApiApp>> | undefined;
+const app = createApiApp();
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  appPromise ??= Promise.resolve(createApiApp());
-  const app = await appPromise;
-  return new Promise<void>((resolve, reject) => {
-    res.once("finish", () => resolve());
-    res.once("close", () => resolve());
-    res.once("error", err => reject(err));
-    try {
-      app(req as never, res as never);
-    } catch (error) {
-      reject(error);
-    }
-  });
-}
+export default app;
